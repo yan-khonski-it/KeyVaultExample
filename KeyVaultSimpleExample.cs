0f1b249e268d6e51487e527beaac56291453b1e2
Yan Khonski <yankhonskiy@gmail.com>
2023-05-06 10:52:00 +0200

    Initial commit

---
+++ KeyVaultSimpleExample.cs
@@ -0,0 +1,9 @@
+﻿namespace yan.learning.KeyVaultExample;
+
+public class KeyVaultSimpleExample
+{
+    public void run()
+    {
+        System.Console.WriteLine("Hello World!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace yan.learning.KeyVaultExample;
+
+class Program
+{
+    static void Main()
+    {
+        KeyVaultSimpleExample keyVaultSimpleExample = new KeyVaultSimpleExample();
+        keyVaultSimpleExample.run();
+    }
+}
